0049a0b3ea12092848855d919479973e548b8634
Samuel Muniz <137716288+DevSamuelMuniz@users.noreply.github.com>
2024-11-05 17:00:22 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import os
 from flask import Flask, request, jsonify
-import sqlite3
+from pymongo import MongoClient
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": ["http://127.0.0.1:5500"]}})  # Substitua pela origem específica
 
-# Conexão com o banco de dados
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+# Conexão com o MongoDB Atlas
+client = MongoClient('mongodb+srv://recinproj:NRdhqU14UA14vJF5@cluster0.r8fkm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client['glowRec']  # Substitua com o nome do banco de dados
+agendamentos_collection = db.agendamentos  # Nome da coleção
 
 # Rota para agendar
 @app.route('/api/agendar', methods=['POST'])
@@ -26,16 +24,20 @@ def agendar():
     if not all([nome, telefone, email, servico, data_agenda, horario]):
         return jsonify({'error': 'Todos os campos são obrigatórios'}), 400
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('''INSERT INTO agendamentos (nome, telefone, email, servico, data_agenda, horario)
-                      VALUES (?, ?, ?, ?, ?, ?)''', (nome, telefone, email, servico, data_agenda, horario))
-    conn.commit()
-    conn.close()
+    # Insere o agendamento no MongoDB
+    agendamento = {
+        'nome': nome,
+        'telefone': telefone,
+        'email': email,
+        'servico': servico,
+        'data_agenda': data_agenda,
+        'horario': horario
+    }
+    agendamentos_collection.insert_one(agendamento)
 
     return jsonify({'message': 'Agendamento criado com sucesso!'}), 201
 
 if __name__ == '__main__':
     # Pega a porta da variável de ambiente PORT ou usa 5000 como padrão
-    port = int(os.environ.get('PORT', 5000))
+    port = 5000
     app.run(host='0.0.0.0', port=port, debug=True)
